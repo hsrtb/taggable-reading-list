1b201f7d352e13a7ad805fe92e560d136861164a
Nibaw Robertson <noscrobertson@gmail.com>
2024-08-01 15:44:04 -0500

    round dates to nearest second

--- background.js
+++ background.js
@@ -4,7 +4,7 @@ async function save_tabs() {
     let ids = []
     for (const tab of tab_array) {
         console.log(tab.title + " - " + tab.url);
-        saves_array.push({url: tab.url, title: tab.title, date: new Date().valueOf()});
+        saves_array.push({url: tab.url, title: tab.title, date: 1000 * Math.round(new Date().valueOf() / 1000)});
         ids.push(tab.id);
     }
     await storageapi.set({saves:saves_array});
