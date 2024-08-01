17035cc0747004ee6f5cd0f037548ebe55c07c1c
Nibaw Robertson <noscrobertson@gmail.com>
2024-08-01 15:56:28 -0500

    save tabs at front instead of back of list

--- background.js
+++ background.js
@@ -1,13 +1,14 @@
 async function save_tabs() {
     let tab_array = await browser.tabs.query({currentWindow:true});
     let saves_array = (await storageapi.get({saves: []})).saves;
-    let ids = []
+    let new_saves_array = [];
+    let ids = [];
     for (const tab of tab_array) {
         console.log(tab.title + " - " + tab.url);
-        saves_array.push({url: tab.url, title: tab.title, date: 1000 * Math.round(new Date().valueOf() / 1000)});
+        new_saves_array.push({url: tab.url, title: tab.title, date: 1000 * Math.round(new Date().valueOf() / 1000)});
         ids.push(tab.id);
     }
-    await storageapi.set({saves:saves_array});
+    await storageapi.set({saves:new_saves_array.concat(saves_array)});
     browser.tabs.remove(ids);
     browser.windows.create({url: "readinglist.html"});
 }
